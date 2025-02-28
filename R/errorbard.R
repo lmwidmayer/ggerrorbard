0ad706ffb86fe3cc26c8c00c2bfe2f8d3bf179c2
Lisa Widmayer <lisa.widmayer@uni-marburg.de>
2025-02-28 14:57:05 +0100

    fix documentation

--- R/errorbard.R
+++ R/errorbard.R
@@ -2,30 +2,29 @@
 #'
 #' @description `GeomErrorBarD` proto type definition based on `ggplot2::GeomSegment`.
 #'
+#'
 #' @export
 GeomErrorbarD <- ggplot2::ggproto("GeomErrorbarD", ggplot2::GeomSegment,
-  required_aes = c("xcenter", "ycenter", "latitude", "angle"),
+  required_aes = c("x", "y", "latitude", "angle"),
   setup_data = function(data, params) {
     # rotate start and end points around center points
     transform(data,
-      xend = xcenter + (cos(angle * pi / 180)) * latitude / 2,
-      yend = ycenter + (sin(angle * pi / 180)) * latitude / 2,
-      x = xcenter - (cos(angle * pi / 180)) * latitude / 2,
-      y = ycenter - (sin(angle * pi / 180)) * latitude / 2
+      xend = x + (cos(angle * pi / 180)) * latitude / 2,
+      yend = y + (sin(angle * pi / 180)) * latitude / 2,
+      x = x - (cos(angle * pi / 180)) * latitude / 2,
+      y = y - (sin(angle * pi / 180)) * latitude / 2
     )
   }
 )
 
 #' @title Diagonal error bar
 #'
-
-#' @param xcenter Aesthetic mapping for the center x coordinate of error bar. Use this instead of `x`.
-#' @param ycenter Aesthetic mapping for the center y coordinate of error bar. Use this instead of `y`.
-#' @param latitude Aesthetic mapping for the total breadth of the error interval. Use this instead of `xmin`, `xmax`, `ymin`, `ymax`.
-#' @param angle Angle of error bars in degrees
+#' @inheritParams ggplot2::geom_segment
 #'
 #' @description
 #' Draw a diagonal (or arbitrarily oriented) error bar in a `ggplot2`, inherits from [ggplot2::geom_segment].
+#'
+#' The error bar is defined by aesthetic mappings for the center x coordinate (`x`), center y coordinate (`y`), total breadth of the error interval (`latitude`), and angle of the error bar in degrees (`angle`).
 #'
 #' @seealso [[ggplot2::geom_segment]]
 #'
